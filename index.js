0f582e085e626c8c0416fcb9e0a61483563d7a58
Sahal V A <sahal.v@axelerant.com>
2024-09-23 14:28:46 +0530

    LDT-796 Exercise: String Properties

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 
+function showProperties(obj) {
+    for (let i in obj) {
+        if (typeof obj[i] === 'string')
+            console.log(i, obj[i]);
+    }
+}
+
+const movie = {
+    title: 'My movie',
+    year: 2024,
+    director: 'Sahal'
+};
+showProperties(movie);
