abbe70349a2067ec08d09e58ec7de78eaaf2e62f
Sahal V A <sahal.v@axelerant.com>
2024-09-23 08:17:08 +0530

    LDT-796 Exercise: Max of two numbers

---
+++ index.js
@@ -0,0 +1,11 @@
+
+// Function to find max of two numbers.
+function findMax(a, b) {
+    return (a > b) ? a : b;
+}
+
+// Find max of 10/2 and assign to number.
+let number = findMax(10, 2);
+
+// Log the max number.
+console.log(number);
