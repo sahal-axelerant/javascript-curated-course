2245e882778ebf76f4520d4ee52f1f3294e4ff2a
Sahal V A <sahal.v@axelerant.com>
2024-09-23 17:34:33 +0530

    LDT-796 Exercise: Price ranges

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+// Create an array of price range objects used to define price range filters.
+// $ - Inexpensive, $$ - Moderate, $$$ - Expensive.
 
+let priceRanges = [
+    { label: "$", tooltip: "Inexpensive", minPerPerson: 0, maxPerPerson: 10 },
+    { label: "$$", tooltip: "Moderate", minPerPerson: 11, maxPerPerson: 20 },
+    { label: "$$$", tooltip: "Expensive", minPerPerson: 21, maxPerPerson: 50 }
+];
+console.log(priceRanges);
