4a78f73023c3e8be7e8ec56b6c5e6e6d9e43af91
Sahal V A <sahal.v@axelerant.com>
2024-09-23 17:48:05 +0530

    LDT-796 Exercise: Array includes

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 
+const array = [1, 3, 5, 7, 9, 0];
+
+console.log("Array:", array);
+console.log(1, includes(array, 1));
+console.log(2, includes(array, 2));
+console.log(10, includes(array, 10));
+console.log(0, includes(array, 0));
+
+function includes(array, search) {
+    for (const iterator of array) {
+        if (iterator === search) return true;
+    }
+    return false;
+}
