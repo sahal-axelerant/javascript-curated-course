f0175135d4933935eff48e892bece3cb950f6770
Sahal V A <sahal.v@axelerant.com>
2024-09-23 18:02:32 +0530

    LDT-796 Exercise: Array move

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 
+const numbers = [1, 2, 3, 4];
+const output = move(numbers, 0, 2);
+console.log(output);
+
+
+function move(array, index, offset) {
+    const position = index + offset;
+    if (position < 0 || position >= array.length) {
+        console.error("Invalid offset")
+    }
+    else {
+        const output = [...array];
+        let element = output.splice(index, 1)[0];
+        output.splice(position, 0, element);
+        return output;
+    }
+}
