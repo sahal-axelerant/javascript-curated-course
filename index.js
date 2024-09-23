058156a23f6fc63720aaf2ec4c06974118fd923c
Sahal V A <sahal.v@axelerant.com>
2024-09-23 17:41:39 +0530

    LDT-796 Exercise: Array from range

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 
+function arrayFromRange(min, max) {
+    let output = [];
+    for (let number = min; number <= max; number++) {
+        output.push(number);
+    }
+    return output;
+}
+
+console.log(arrayFromRange(-3, 3));
