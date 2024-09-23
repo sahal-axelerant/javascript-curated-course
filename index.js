6cf3d0b1c146909f68d8b85069998a7baca4b8d2
Sahal V A <sahal.v@axelerant.com>
2024-09-23 20:06:16 +0530

    LDT-796 Exercise: Array Get max

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 
+const numbers = [1, 2, 3, 4, 10, 15, 1, 1];
+const output = getMax(numbers);
+const output1 = getMaxWithReduce(numbers);
+console.log(output);
+console.log(output1);
+
+
+function getMax(array) {
+    if (array.length === 0) return undefined;
+    let max = array[0];
+    for (const value of array) {
+        if (value > max) max = value;
+    }
+    return max;
+}
+
+function getMaxWithReduce(array) {
+    if (array.length === 0) return undefined;
+    return array.reduce((a, b) => (a > b) ? a : b);
+}
