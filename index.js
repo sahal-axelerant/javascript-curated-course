428f128c102275dd5d366dba60872fd3d805fc39
Sahal V A <sahal.v@axelerant.com>
2024-09-23 20:31:39 +0530

    LDT-796 Exercise: Area of circle

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 
+
+let circle = {
+    radius: 1,
+    area: 0,
+    get area() {
+        return Math.PI * this.radius * this.radius;
+    }
+};
+
+console.log(circle.radius);
+console.log(circle.area);
