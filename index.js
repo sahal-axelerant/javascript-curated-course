231a80c77a5a0ee7f1957f4c2abfae6c310e620d
Sahal V A <sahal.v@axelerant.com>
2024-09-23 17:27:11 +0530

    LDT-796 Exercise: Constructor functions

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 
+// Create a blog post object that has the properties
+// title, body, author, views(number), comments (author, body), isLive(boolean).
+
+function Blog(title, body, author) {
+    this.title = title,
+    this.body = body,
+    this.author = author,
+    this.views = 0,
+    this.comments = [],
+    this.isLive = false
+}
+
+const blog = new Blog("Blog title","Blog body hsbfjbdjfh","Blog Author");
+
+
+console.log(blog);
