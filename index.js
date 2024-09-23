47ac430f519daca1672a87addc9a30b7f93dcaf7
Sahal V A <sahal.v@axelerant.com>
2024-09-23 15:31:12 +0530

    LDT-796 Exercise: Prime numbers

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 
+showPrimes(30);
+
+function showPrimes(limit) {
+    for (let number = 2; number <= limit; number++) {
+        if (isPrime(number)) {
+            console.log(number);
+        }
+    }
+}
+
+function isPrime(number) {
+    for (let i = 2; i < number; i++) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
