e41f5883296c9b5909695f2fce7a232e51b87301
Sahal V A <sahal.v@axelerant.com>
2024-09-23 15:49:47 +0530

    LDT-796 Exercise: Factory functions and Constructor functions

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 
+// Factory fn
+function createAddress(city, state, zipcode) {
+    return {
+        city,
+        state,
+        zipcode
+    }
+}
+
+// Constructor fn.
+function Address(city, state, zipcode) {
+    this.city = city;
+    this.state = state;
+    this.zipcode = zipcode;
+}
+
+let myAddress = createAddress('Kochi', 'Kerala', 688535);
+console.log(myAddress);
+
+let myWorkAddress = new Address('Bangalore', 'Karnataka', 688535);
+console.log(myWorkAddress);
