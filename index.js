36bf7d50c33d19d519a68fd72129f22ab899caac
Sahal V A <sahal.v@axelerant.com>
2024-09-23 17:18:05 +0530

    LDT-796 Exercise: Blog post object

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 
+// Create a blog post object that has the properties
+// title, body, author, views(number), comments (author, body), isLive(boolean).
+
+const blog = {
+    title: "Blog title",
+    body: "Blog body hsbfjbdjfh",
+    author: "Blog Author",
+    views: 15,
+    comments: [
+        { author: 'a', body: 'b' },
+        { author: 'c', body: 'd' }
+    ],
+    isLive: true
+};
+
+console.log(blog);
