b7f482487040457ca89e7610b037c0dc4bfe5b48
Sahal V A <sahal.v@axelerant.com>
2024-09-23 15:16:54 +0530

    LDT-796 Exercise: Show stars

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 
+showStars(10);
+
+function showStars(rows) {
+    let stars = '';
+    for (let index = 1; index <= rows; index++) {
+        for (let inner_index = 1; inner_index <= index; inner_index++) {
+            stars += '*';
+        }
+        stars += "\n";
+    }
+    console.log(stars);
+}
