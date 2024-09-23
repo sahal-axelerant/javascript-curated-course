78034e421d942995d0b95ec953780ace704b5d7c
Sahal V A <sahal.v@axelerant.com>
2024-09-23 10:28:11 +0530

    LDT-796 Exercise: Even and Odd numbers

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 
+function showNumbers(limit) {
+    let message;
+    for (let i = 1; i <= limit; i++) {
+        message = (i % 2 === 0) ? 'EVEN' : 'ODD';
+        console.log(i, message);
+    }
+}
+
+showNumbers(25);
