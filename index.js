98629d1bf92f88fab214522fa9c3cab1a43a099c
Sahal V A <sahal.v@axelerant.com>
2024-09-23 18:15:59 +0530

    LDT-796 Exercise: Array occurences

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 
+const numbers = [1, 2, 3, 4, 1, 1, 1, 1];
+const output = countOcuurences(numbers, 1);
+const output1 = countOcuurencesWithReduce(numbers, 1);
+console.log(output);
+console.log(output1);
+
+
+function countOcuurences(array, search) {
+    let count = 0;
+    for (const value of array) {
+        if (value === search) count++
+    }
+    return count;
+}
+
+function countOcuurencesWithReduce(array, search) {
+    return array.reduce((accumulator, current) => {
+        const occurence = (current === search) ? 1 : 0;
+        return accumulator + occurence;
+    }, 0);
+}
