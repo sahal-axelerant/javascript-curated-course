bbfbcefd7343b56ceaa7527f2ec417ef2cd860aa
Sahal V A <sahal.v@axelerant.com>
2024-09-23 15:38:13 +0530

    LDT-796 Exercise: Address object

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 
+function showAddress(address) {
+    for (const key in address) {
+        console.log(key, address[key]);
+    }
+}
+
+let address = {
+    city: "Kochi",
+    state: "Kerala",
+    zip: 688535
+};
+showAddress(address);
