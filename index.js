fe30bd303f5903eb29573b4bd7dc4bfce8d5bac7
Sahal V A <sahal.v@axelerant.com>
2024-09-23 11:34:43 +0530

    LDT-796 Exercise: Count truthy

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+// Count the number of truthy values in an array.
+function countTruthy(array) {
+    let count = 0;
+    for (let value of array) {
+        // If value is truthy, condition returns true and increase count.
+        if (value) {
+            count += 1;
+        }
+    }
+    return count;
+}
 
+const values = [1, '', 2, undefined, {name: 'Sahal'}, NaN, '1', true, 'Axelerant', 1000];
+console.log(countTruthy(values));
