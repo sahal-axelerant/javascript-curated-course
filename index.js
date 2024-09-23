33d765807a8306da00c2089fc2350d1c5b7b3b1d
Sahal V A <sahal.v@axelerant.com>
2024-09-23 14:35:01 +0530

    LDT-796 Exercise: Sum of multiples of 3 & 5

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 
+function sum(limit) {
+    let sum = 0;
+    for (let i = 1; i <= limit; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+// Log sum of multiples of 3 and 5 from 1 ... 10.
+console.log(sum(10));
