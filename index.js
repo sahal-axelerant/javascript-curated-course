ac17c57bf5c275180d14aabdcf1681037bb6b945
Sahal V A <sahal.v@axelerant.com>
2024-09-25 07:21:51 +0530

    LDT-797 Exercise: Stopwatch

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 
+function Stopwatch() {
+    let duration = 0;
+    let running = false;
+    let start_time = 0;
+    let stop_time = 0;
+    this.start = function () {
+        if (running) {
+            console.error("Stopwatch has already started.");
+            return;
+        }
+        running = true;
+        start_time = new Date();
+    }
+    this.stop = function () {
+        if (!running) {
+            console.error("Stopwatch is not started.");
+            return;
+        }
+        running = false;
+        stop_time = new Date();
+        let sec = (stop_time.getTime() - start_time.getTime()) / 1000;
+        duration += sec;
+    }
+    this.reset = function () {
+        duration = 0;
+        running = false;
+    }
+    Object.defineProperty(this, 'duration', {
+        get: function() { return duration; }
+    });
+}
+
+const stopwatch = new Stopwatch();
