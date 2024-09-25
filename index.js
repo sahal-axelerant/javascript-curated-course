70dccf99dc1c45814b30df69b4771ecdcd3529d6
Sahal V A <sahal.v@axelerant.com>
2024-09-25 10:22:18 +0530

    LDT-797 Exercise: Prototypical Inheritance

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 
+// Parent constructor.
+function HtmlElement() {
+    this.click = function() { console.log("Clicked"); }
+}
+
+HtmlElement.prototype.focus = function() { console.log("Focused"); }
+HtmlElement.prototype.constructor = HtmlElement;
+
+
+const e = new HtmlElement();
+
+
+// Child constructor.
+function HtmlSelectElement(items = []) {
+    this.items = items;
+    this.addItem = function(value) {
+        this.items.push(value);
+    }
+    this.removeItem = function(value) {
+        this.items = this.items.filter(item => item !== value);
+    }
+}
+
+HtmlSelectElement.prototype = new HtmlElement();
+HtmlSelectElement.prototype.constructor = HtmlSelectElement;
+
+const s = new HtmlSelectElement(['1', '2']);
+s.click();
+s.focus();
+s.addItem('3');
+s.addItem('4');
+s.removeItem('4');
