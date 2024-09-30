bb35a3b69457d9eb830c4f971864a2c34d8edd90
Sahal V A <sahal.v@axelerant.com>
2024-09-30 10:25:17 +0530

    LDT-797 Exercise: Stack

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 
+'use strict';
+
+// Stack class with push, peek and pop menthods.
+const _stackItems = new WeakMap();
+class Stack {
+    constructor () {
+        _stackItems.set(this, []);
+    }
+
+    // Push method.
+    push(item) {
+        const items = _stackItems.get(this);
+        items.push(item);
+    }
+
+    // Peek method.
+    peek() {
+        const items = _stackItems.get(this);
+        if (items.length === 0) throw new Error('Stack is empty');
+        console.log(items.slice(-1)[0]);
+    }
+
+    // Pop method.
+    pop() {
+        const items = _stackItems.get(this);
+        if (items.length === 0) throw new Error('Stack is empty');
+        return items.pop();
+    }
+
+    get count() {
+        return _stackItems.get(this).length;
+    }
+}
+
+const stack = new Stack();
+console.log(stack);
