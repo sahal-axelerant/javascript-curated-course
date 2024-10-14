8902cd70c799fc2edeecaefd23a47504786df87d
Sahal V A <sahal.v@axelerant.com>
2024-10-14 12:55:11 +0530

    Exercise 1: Create a Simple API with Callbacks

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 
+function getUser(userId) {
+    const user = fetch('https://jsonplaceholder.typicode.com/todos/' + userId)
+    .then(response =>  response.json());
+    return user;
+}
+
+const user = getUser(1);
+console.log("Fetched user:", user);
